82ab8e4605ed30add7fb410977762e9dd2d1c035
vigneshwarane89 <vigneshwarane89@gmail.com>
2020-07-24 08:06:55 +0530

    Simple Store app Website is completed.

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,9 @@ from . import views
 urlpatterns = [
     path('',views.catalog,name="catalog"),
     path('cart/',views.cart,name="cart"),
-    path('cart/remove/',views.removeFromCart,name="remove")
+    path('cart/remove/',views.removeFromCart,name="remove"),
+    path('cart/checkout/',views.checkout,name="checkout"),
+    path('cart/checkout/complete/',views.completeOrder,name="completeOrder"),
+    path('admin-login/', views.adminLogin, name='admin_login'),
+    path('admin-panel/', views.adminDashboard, name='admin'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from store.models import Product
+from store.models import Product,Order
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def cartItems(cart):
     items=[]
@@ -25,6 +27,15 @@ def priceCart(cart):
     for item in cart_items:
         price+=item.price
     return price
+
+def genItemsList(cart):
+    cart_items = cartItems(cart)
+    items_list = ""
+    for item in cart_items:
+        items_list += ","
+        items_list += item.name
+    return items_list
+
 
 
 
@@ -56,4 +67,55 @@ def removeFromCart(request):
     obj_index=request.session['cart'].index(obj_to_remove)
 
     request.session['cart'].pop(obj_index)
+
     return redirect('cart')
+
+def checkout(request):
+    request.session.set_expiry(0)
+    cart=request.session['cart']
+    ctx={
+    'cart':cart,
+    'cart_size':len(cart),
+    'cart_items':cartItems(cart),
+    'total':priceCart(cart)
+    }
+    return render(request,"checkout.html",ctx)
+
+def completeOrder(request):
+    cart = request.session['cart']
+    request.session.set_expiry(0)
+    ctx = {'cart':cart, 'cart_size':len(cart), 'cart_items':cartItems(cart), 'total_price': priceCart(cart)}
+    order = Order()
+    order.items = genItemsList(cart)
+    order.first_name = request.POST['first_name']
+    order.last_name = request.POST['last_name']
+    order.address = request.POST['address']
+    order.city = request.POST['city']
+    order.payment_data = request.POST['payment_data']
+    order.fulfill = False
+    order.payment_method = request.POST['payment']
+    order.save()
+    request.session['cart'] = []
+    return render(request, "complete_order.html", ctx)
+
+def adminLogin(request):
+    if request.method == "POST":
+        usname = request.POST["username"]
+        pwd = request.POST["password"]
+        user = authenticate(username=usname, password=pwd)
+        if user is not None:
+                login(request, user)
+                return redirect("admin")
+        else:
+            return render(request, "admin_login.html", {'login': False})
+
+
+    return render(request, "admin_login.html",None)
+
+
+
+@login_required
+def adminDashboard(request):
+    orders = Order.objects.all()
+    ctx = {'orders': orders}
+    return render(request, "admin_panel.html", ctx)
